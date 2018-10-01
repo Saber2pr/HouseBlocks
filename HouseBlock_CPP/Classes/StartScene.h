7d622da695dcb3ad096f649dfaa984146c7c7817
saber2pr <saber2pr@gmail.com>
2018-10-01 13:24:21 +0800

    movetoClassDir

---
+++ HouseBlock_CPP/Classes/StartScene.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "cocos2d.h"
+#include "ui/CocosGUI.h"
+#include "SceneInterface.h"
+USING_NS_CC;
+
+class StartScene: public cocos2d::Scene, SceneInterface
+{
+private:
+	Size _size;
+	Sprite* _background = nullptr;
+	Label* _title = nullptr;
+	ui::Button* _startBtn = nullptr;
+public:
+	StartScene();
+	~StartScene();
+	CREATE_FUNC(StartScene);
+	static Scene* createScene();
+	virtual bool init();
+	bool initView();
+	bool initUI();
+};
+
