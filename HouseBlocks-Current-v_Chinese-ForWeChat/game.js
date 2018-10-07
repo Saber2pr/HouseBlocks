5bfa803351472377df2a9d5f94be2930f8fcfc94
saber2pr <saber2pr@gmail.com>
2018-10-07 10:18:04 +0800

    deleteWeChat

--- HouseBlocks-Current-v_Chinese-ForWeChat/game.js
+++
@@ -1,9 +0,0 @@
-require('libs/weapp-adapter-min');
-
-// 这里需要先加载微信脚本，不然 window 对象会报错
-window.REMOTE_SERVER_ROOT = '';//远程服务器地址
-
-var Parser = require('libs/xmldom/dom-parser');
-window.DOMParser = Parser.DOMParser;
-require('weapp-adapter');
-require('game.min');
